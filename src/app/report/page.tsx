5836f5f6c8d6510f7f64b9f98502bd2b9c07aec4
twadayama <takahiro.wadayama@gmail.com>
2024-11-05 12:46:31 +0900

    update for deploy4

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -336,10 +336,8 @@ const ReportPage = () => {
         </table>
         </div>
 
-        <Link href="/" legacyBehavior>
-          <a className="w-full bg-gray-800 text-white py-2 rounded-md hover:bg-gray-900 mt-6 text-center block">
-            戻る
-          </a>
+        <Link href="/" className="w-full bg-gray-800 text-white py-2 rounded-md hover:bg-gray-900 mt-6 text-center block">
+          戻る
         </Link>
       </div>
     </div>
